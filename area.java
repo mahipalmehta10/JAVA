afd593c6c7ac8ee252fae29947b8e4da3e550f8c
Mahipalmehta10 <mahipalmehta11@gmail.com>
2023-07-02 00:15:08 +0530

    1

---
+++ area.java
@@ -0,0 +1,12 @@
+
+public class area {
+    public static void main(String[] args) {
+        double area ;
+        int r=28;
+        double pi=3.14;
+        area = pi*r*r;
+        System.out.println("the area is:"+ area);  
+
+    }
+    
+}
