f952560dc23284aa006c8683a2d4ed1ba88fd669
Mahipalmehta10 <mahipalmehta11@gmail.com>
2023-07-04 20:22:35 +0530

    typecasting

---
+++ typecasting.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class typecasting {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        char a ='a';
+        char b ='b';
+        char m ='i';
+        System.out.println((int) a);
+        System.out.println((int) m);
+        System.out.println(a+b);
+        sc.close();
+    }
+}
---
+++ typeconversion.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class typeconversion {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        // converting int to float
+        // int num = sc.nextFloat();
+        // System.out.println(num);
+        // converting  float to int
+         float number = sc.nextInt();
+         System.out.println(number);  
+         sc.close();      
+    }
+    
+}
