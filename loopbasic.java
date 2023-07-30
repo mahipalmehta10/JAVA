3a8f4cafcf382b2c57199ec2e66481bf9ea94a08
Mahipalmehta10 <mahipalmehta11@gmail.com>
2023-07-30 23:25:13 +0530

    basic

---
+++ loopbasic.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class loopbasic {
+    public static void main(String[] args) {
+    Scanner sc =new Scanner(System.in);
+    System.out.println("enter the no ");
+    int num =sc.nextInt();
+    while(num<=100){
+        System.out.println("Hello World");
+        num++;
+    }
+    sc.close();
+    }
+    
+}
